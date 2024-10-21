0b22f3d6ddeb2f39a3405ad4aa602b74528f8368
Marcio Silva <m.s.copr@gmail.com>
2024-10-21 16:10:37 -0300

    :sparkles: ComercialAPI - Adicionado ao projeto as classes: TLibary e TModelFireDAC.

--- ComercialAPI.dpr
+++ ComercialAPI.dpr
@@ -12,7 +12,9 @@ uses
   uModel.Abstraction.DataManager in 'src\model\uModel.Abstraction.DataManager.pas',
   uModel.Abstraction.Statement in 'src\model\uModel.Abstraction.Statement.pas',
   uModel.Abstraction.Repository in 'src\model\uModel.Abstraction.Repository.pas',
-  uModel.FireDACEngineException in 'src\model\uModel.FireDACEngineException.pas';
+  uModel.FireDACEngineException in 'src\model\uModel.FireDACEngineException.pas',
+  uLibary in 'src\uLibary.pas',
+  uModel.FireDAC in 'src\model\uModel.FireDAC.pas';
 
 begin
   try
