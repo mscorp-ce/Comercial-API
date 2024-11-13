fa75bb366ef3d0824405e17d68eb460d3f304f3c
Marcio Silva <m.s.copr@gmail.com>
2024-11-13 06:13:25 -0300

    :sparkles: Modificado uModel.Abstraction.Repository
    - Removido métodos comentados.
    
    A interface IRepository<T> passa a ficar com os seguintes métodos:
    
    - GetProperties: para obter propriedades de um objeto T a partir de uma instrução.
    
    - SetProperties: para definir propriedades de um objeto T a partir de uma instrução.
    
    - ListAll: para listar todos os objetos T com parâmetros opcionais.
    
    - GetById: para recuperar um objeto T pelo ID.
    
    - Save: para salvar um objeto T e retornar um JSON com o resultado.
    
    - Merge: para mesclar um objeto T com um repositório existente.
    
    - Remove: para remover um objeto T do repositório.
    
     GetRecords: para obter a contagem de registos com base nos parâmetros fornecidos.

--- src/model/uModel.Abstraction.Repository.pas
+++ src/model/uModel.Abstraction.Repository.pas
@@ -3,32 +3,26 @@ unit uModel.Abstraction.Repository;
 interface
 
 uses
-  System.Classes, Data.DB, System.Generics.Collections, uModel.Abstraction.Statement;
+  System.Classes, Data.DB, System.Generics.Collections, uModel.Abstraction.Statement,
+  System.JSON;
 
 type
+  IStreamRepository<T: class> = interface
+    ['{7268709A-3C28-4C25-9048-16554B626A26}']
+    function SavePhoto(const AEntity: T; const Value: TStream): Boolean;
+    function GetPthoById(const AEntity: T): TMemoryStream;
+  end;
+
   IRepository<T: class> = interface
   ['{557CB439-8E45-4FAF-AEF4-1ADBE83590C6}']
-    function Fields: TStrings;
-    procedure SetStatement(Statement: IStatement; Entity: T);
-    procedure SetProperty(Statement: IStatement; Entity: T);
-    function GeneratedValue: Integer;
-    function CurrentGeneratedValue: Integer;
-    function Save(Entity: T): Boolean;
-    procedure AfterSave(Entity: T);
-    function Update(Entity: T): Boolean; overload;
-    function Update(CommandSQL: String; Parameter: String; Entity: T): Boolean; overload;
-    function DeleteById(Entity: T): Boolean;
-    function FindById(Id: Integer): T;
-    function FindExists: Boolean; overload;
-    function FindExists(CommadSQL: String; Parameter: String;
-      ParameterType: TFieldType; Value: Variant): IStatement; overload;
-    function FindAll: TObjectList<T>; overload;
-    function FindAll(CommadSQL: String): TObjectList<T>; overload;
-    function FindAll(CommadSQL: String; Entity: T): TObjectList<T>; overload;
-    function Frist: T;
-    function Previous(Id: Integer): T;
-    function Next(Id: Integer): T;
-    function Last: T;
+    procedure GetProperties(var AEntity: T; const AStatement: IStatement);
+    procedure SetProperties(var AStatement: IStatement; const AEntity: T);
+    function ListAll(const AParams: TDictionary<string, string>): TObjectList<T>;
+    function GetById(const AId: Integer): T;
+    function Save(var AEntity: T): TJSONObject;
+    function Merge(var AEntity: T): Boolean;
+    function Remove(const AEntity: T): Boolean;
+    function GetRecords(const AParams: TDictionary<string, string>): Int64;
   end;
 
 implementation
